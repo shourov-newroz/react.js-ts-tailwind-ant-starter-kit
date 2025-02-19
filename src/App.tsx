d1c01207e6fb2d7ed9889047f116f4609cff099e
Shouov Saha <shorov@newroztech.com>
2025-02-19 11:18:30 +0600

    react.js-ts-tailwind-ant-starter-kit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from 'antd';
+
+const App = () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}
+  >
+    <Button type='primary' size='large'>
+      text
+    </Button>
+  </div>
+);
+
+export default App;
